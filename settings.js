a9ba16857a4b57b3e90e08d835d3580b7d45d6e9
Aric Beagley <aric.beagley@gmail.com>
2017-08-12 13:58:19 -0600

    Testing prompts

---
+++ settings.js
@@ -0,0 +1,10 @@
+module.exports = {
+  prompts: {
+    type: 'input',
+    name: 'projectName',
+    message: 'What do you want to call your project',
+    validate: function (userInput) {
+      return (userInput && userInput !== '') ? true : 'Project name is required'
+    }
+  }
+}
