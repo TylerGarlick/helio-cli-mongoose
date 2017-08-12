623e525e6f4fbf1b5518978cdc6361938aef30b8
Aric Beagley <aric.beagley@gmail.com>
2017-08-12 14:05:32 -0600

    Update prompts

--- settings.js
+++ settings.js
@@ -1,10 +1,12 @@
 module.exports = {
-  prompts: {
-    type: 'input',
-    name: 'projectName',
-    message: 'What do you want to call your project',
-    validate: function (userInput) {
-      return (userInput && userInput !== '') ? true : 'Project name is required'
+  prompts: [
+    {
+      type: 'input',
+      name: 'projectName',
+      message: 'What do you want to call your project',
+      validate: function (userInput) {
+        return (userInput && userInput !== '') ? true : 'Project name is required'
+      }
     }
-  }
+  ]
 }
